85955e3189d1aa37c2d57c6ccb6ae4712e8544f0
shoproizoshlo <iamsusunny@gmail.com>
2024-07-31 12:39:04 +0200

    set up navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,20 @@
+import NavSearch from "./NavSearch";
+import LinksDropdown from "./LinksDropdown";
+import DarkMode from "./DarkMode";
+import Logo from "./Logo";
+
 function Navbar() {
-  return <h1 className="text-3xl">Navbar</h1>;
+  return (
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row sm: justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default Navbar;
