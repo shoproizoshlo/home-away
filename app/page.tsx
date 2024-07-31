564575e4b54e808d800d5d03f9a96a80238e5b2c
shoproizoshlo <iamsusunny@gmail.com>
2024-07-31 12:15:04 +0200

    added Shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1 className="text-xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 }
