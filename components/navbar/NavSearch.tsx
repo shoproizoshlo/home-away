ad367b9031c2998892ac656b848017a6e9a1b293
shoproizoshlo <iamsusunny@gmail.com>
2024-07-31 12:46:37 +0200

    set up logo and input search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,4 +1,13 @@
+import { Input } from "../ui/input";
+
 function NavSearch() {
-  return <h1 className="text-3xl">NavSearch</h1>;
+  return (
+    <Input
+      type="search"
+      placeholder="find a property"
+      className="max-w-xs dark:bg-muted "
+    />
+  );
 }
+
 export default NavSearch;
