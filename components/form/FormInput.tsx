745fca90ddd422b855db85734557fabf522d8f5c
shoproizoshlo <iamsusunny@gmail.com>
2024-08-01 13:03:11 +0200

    FormInput Component

---
+++ components/form/FormInput.tsx
@@ -0,0 +1,27 @@
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+type Props = {
+  name: string;
+  type: string;
+  label?: string;
+  defaultValue?: string;
+  placeholder?: string;
+};
+export default function FormInput(props: Props) {
+  const { name, type, label, defaultValue, placeholder } = props;
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {label || name}
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
