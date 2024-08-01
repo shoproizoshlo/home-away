b58a128171a59d993e8de2dfbe2ff27cf7e18e68
shoproizoshlo <iamsusunny@gmail.com>
2024-08-01 17:08:54 +0200

    SubmitButton Component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -12,15 +12,7 @@ function CreateProfilePage() {
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
       <div className="border p-8 rounded-md max-w-lg">
-        <form action={createProfileAction}>
-          <div className="mb-2">
-            <Label htmlFor="firstName">First name</Label>
-            <Input id="firstName" name="firstName" type="text" />
-          </div>
-          <Button type="submit" size="lg">
-            Create Profile
-          </Button>
-        </form>
+        <form action={createProfileAction}></form>
       </div>
     </section>
   );
