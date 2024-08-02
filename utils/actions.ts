7f7307b88096fe13170d5bf21b76a6a2943e1492
shoproizoshlo <iamsusunny@gmail.com>
2024-08-02 11:16:19 +0200

    Fetch User Profile

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,15 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { profileSchema } from "./schemas";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+
+  if (!user) throw new Error("You must be loged in to access this route");
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+
+  return user;
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -51,4 +60,22 @@ export const fetchProfileImage = async () => {
     },
   });
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      ckerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "Update profile action" };
 };
