a0f379f7c70c93a4416fe02fb987a7f31f821954
shoproizoshlo <iamsusunny@gmail.com>
2024-08-02 10:58:45 +0200

    Fetch Profile Image Action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,13 @@
+import { fetchProfileImage } from "@/utils/actions";
 import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <img src={profileImage} className="w-6 h-6 bg-primary rounded-full" />
+    );
+  }
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
