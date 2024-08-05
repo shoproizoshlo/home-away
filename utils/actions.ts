60515225f2fbf58df86b3fa9205609dbb9003c76
shoproizoshlo <iamsusunny@gmail.com>
2024-08-05 16:30:38 +0200

    ValidateWithZodSchema - Helper Function

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,7 +3,7 @@ import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { profileSchema } from "./schemas";
+import { profileSchema, validateWithZodSchema } from "./schemas";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -30,7 +30,7 @@ export const createProfileAction = async (
     if (!user) throw new Error("Please login to create a profile");
 
     const rawData = Object.fromEntries(formData);
-    const validatedFields = profileSchema.parse(rawData);
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.create({
       data: {
@@ -85,18 +85,13 @@ export const updateProfileAction = async (
   const user = await getAuthUser();
   try {
     const rawData = Object.fromEntries(formData);
-
-    const validatedFields = profileSchema.safeParse(rawData);
-    if (!validatedFields.success) {
-      const errors = validatedFields.error.errors.map((error) => error.message);
-      throw new Error(errors.join(", "));
-    }
+    const validatedFields = validateWithZodSchema(profileSchema, rawData);
 
     await db.profile.update({
       where: {
         clerkId: user.id,
       },
-      data: validatedFields.data,
+      data: validatedFields,
     });
     revalidatePath("/profile");
     return { message: "Profile updated successfully" };
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -13,3 +13,16 @@ export const profileSchema = z.object({
     .string()
     .min(2, { message: "Username must be at least 2 charecters" }),
 });
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+
+    throw new Error(errors.join(", "));
+  }
+  return result.data;
+}
