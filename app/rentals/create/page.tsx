12d38d1052fb0f7a3b87131e8b4d73c3ab277470
shoproizoshlo <iamsusunny@gmail.com>
2024-08-06 18:21:13 +0200

    Counter Input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -7,6 +7,7 @@ import CategoriesInput from "@/components/form/CategoriesInput";
 import TextAreaInput from "@/components/form/TextAreaInput";
 import CountriesInput from "@/components/form/CountriesInput";
 import ImageInput from "@/components/form/ImageInput";
+import CounterInput from "@/components/form/CounterInput";
 
 function CreateProperty() {
   return (
@@ -41,6 +42,14 @@ function CreateProperty() {
             <CountriesInput />
             <ImageInput />
           </div>
+          <h3 className="text-lg mt-8 mb-4 font-medium">
+            Accommodation Details
+          </h3>
+          <CounterInput detail="guests" />
+          <CounterInput detail="bedrooms" />
+          <CounterInput detail="beds" />
+          <CounterInput detail="baths" />
+
           <SubmitButton
             text="create rental"
             className="mt-12"
