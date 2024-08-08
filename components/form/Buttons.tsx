6664f314a0d4fd613ae59bf72238dc5126a38440
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 17:52:06 +0200

    Favorites Toggle Form

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -56,3 +56,23 @@ export const CardSignInButton = () => {
     </SignInButton>
   );
 };
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      size="icon"
+      variant="outline"
+      className=" p-2 cursor-pointer"
+    >
+      {pending ? (
+        <ReloadIcon className=" animate-spin" />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
+};
--- utils/actions.ts
+++ utils/actions.ts
@@ -200,6 +200,35 @@ export const fetchFavoriteId = async ({
   });
   return favorite?.id || null;
 };
-export const toggleFavoriteAction = async () => {
+
+export const toggleFavoriteAction = async (prevState: {
+  propertyId: string;
+  favoriteId: string | null;
+  pathname: string;
+}) => {
+  // const user = await getAuthUser();
+  const { propertyId, favoriteId, pathname } = prevState;
+  console.log(propertyId, favoriteId, pathname);
   return { message: "toggle favorite" };
+
+  // try {
+  //   if (favoriteId) {
+  //     await db.favorite.delete({
+  //       where: {
+  //         id: favoriteId,
+  //       },
+  //     });
+  //   } else {
+  //     await db.favorite.create({
+  //       data: {
+  //         propertyId,
+  //         profileId: user.id,
+  //       },
+  //     });
+  //   }
+  //   revalidatePath(pathname);
+  //   return { message: favoriteId ? "Removed from Faves" : "Added to Faves" };
+  // } catch (error) {
+  //   return renderError(error);
+  // }
 };
