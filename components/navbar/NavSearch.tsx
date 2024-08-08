136afbd502ef14671a98da34c4978b5f680a2667
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 17:11:00 +0200

    NavSearch Component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,44 @@
+"use client";
 import { Input } from "../ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <Input
       type="search"
-      placeholder="find a property"
+      placeholder="find a property..."
       className="max-w-xs dark:bg-muted "
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
