ec0b707f4ed1d8639f9325b76950802da1bbc79f
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 15:15:12 +0200

    Home Page - Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import { Button } from "@/components/ui/button";
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
 
-export default function Home() {
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
   return (
-    <div>
-      <h1 className="text-xl">HomePage</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        Click me
-      </Button>
-    </div>
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <PropertiesContainer
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+    </section>
   );
 }
+export default HomePage;
---
+++ components/home/CategoriesList.tsx
@@ -0,0 +1,16 @@
+
+import { categories } from "@/utils/categories";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
+import Link from "next/link";
+
+export default function CategoriesList({
+    category,
+    search,
+}: {
+    category?: string;
+    search?: string;
+}) {
+    return (
+        <div></div>
+    )
+}
