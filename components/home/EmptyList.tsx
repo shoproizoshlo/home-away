c17bd8951a028974d3ca68c61d9e143cd208921f
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 15:29:10 +0200

    Empty List

---
+++ components/home/EmptyList.tsx
@@ -0,0 +1,23 @@
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+function EmptyList({
+  heading = "No items in the list.",
+  message = "Keep exploring our properties.",
+  btnText = "back home",
+}: {
+  heading?: string;
+  message?: string;
+  btnText?: string;
+}) {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-bold ">{heading}</h2>
+      <p className="text-lg">{message}</p>
+      <Button asChild className="mt-4 capitalize" size="lg">
+        <Link href="/">{btnText}</Link>
+      </Button>
+    </div>
+  );
+}
+export default EmptyList;
