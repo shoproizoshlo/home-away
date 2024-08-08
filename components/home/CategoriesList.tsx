e4ae4fa92cd99ef1465010f56a3a0b2eac4d92ee
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 15:25:35 +0200

    Categories List

--- components/home/CategoriesList.tsx
+++ components/home/CategoriesList.tsx
@@ -1,16 +1,40 @@
-
 import { categories } from "@/utils/categories";
 import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 import Link from "next/link";
 
 export default function CategoriesList({
-    category,
-    search,
+  category,
+  search,
 }: {
-    category?: string;
-    search?: string;
+  category?: string;
+  search?: string;
 }) {
-    return (
-        <div></div>
-    )
+  const searchTerm = search ? `&search=${search}` : "";
+  return (
+    <section>
+      <ScrollArea className="py-6">
+        <div className="flex gap-x-4">
+          {categories.map((item) => {
+            const isActive = item.label === category;
+            return (
+              <Link
+                key={item.label}
+                href={`/?category=${item.label}${searchTerm}`}
+              >
+                <article
+                  className={`p-3 flex flex-col items-center cursor-pointer duration-300  hover:text-primary w-[100px] ${
+                    isActive ? "text-primary" : ""
+                  }`}
+                >
+                  <item.icon className="w-8 h-8 " />
+                  <p className="capitalize text-sm mt-1">{item.label}</p>
+                </article>
+              </Link>
+            );
+          })}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </section>
+  );
 }
---
+++ components/home/PropertiesList.tsx
@@ -0,0 +1,13 @@
+import { categories } from "@/utils/categories";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
+import Link from "next/link";
+
+export default function PropertiesList({
+  category,
+  search,
+}: {
+  category?: string;
+  search?: string;
+}) {
+  return <div></div>;
+}
