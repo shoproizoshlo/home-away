5c1c8cbf3a036e5192c78d7ed195fdce43da4531
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 15:39:26 +0200

    PropertyCard - Setup

--- components/home/PropertiesList.tsx
+++ components/home/PropertiesList.tsx
@@ -1,13 +1,13 @@
-import { categories } from "@/utils/categories";
-import { ScrollArea, ScrollBar } from "../ui/scroll-area";
-import Link from "next/link";
+import PropertyCard from "../card/PropertyCard";
+import type { PropertyCardProps } from "@/utils/types";
 
-export default function PropertiesList({
-  category,
-  search,
-}: {
-  category?: string;
-  search?: string;
-}) {
-  return <div></div>;
+function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
+  return (
+    <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
+      {properties.map((property) => {
+        return <PropertyCard key={property.id} property={property} />;
+      })}
+    </section>
+  );
 }
+export default PropertiesList;
