b01743170735b6fbc4ec27f874a79437124a6966
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 12:04:00 +0200

    Fetch Properties

--- utils/actions.ts
+++ utils/actions.ts
@@ -129,6 +129,36 @@ export const updateProfileImageAction = async (
   }
 };
 
+export const fetchProperties = async ({
+  search = "",
+  category,
+}: {
+  search?: string;
+  category?: string;
+}) => {
+  const properties = await db.property.findMany({
+    where: {
+      category,
+      OR: [
+        { name: { contains: search, mode: "insensitive" } },
+        { tagline: { contains: search, mode: "insensitive" } },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      tagline: true,
+      country: true,
+      image: true,
+      price: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return properties;
+};
+
 export const createPropertyAction = async (
   prevState: any,
   formData: FormData
