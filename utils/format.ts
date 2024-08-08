1874e04dafa7e33c2d0a04431d0c1daa7a20e9c9
shoproizoshlo <iamsusunny@gmail.com>
2024-08-08 15:59:06 +0200

    PropertyCard - Structure

---
+++ utils/format.ts
@@ -0,0 +1,9 @@
+export const formatCurrency = (amount: number | null) => {
+  const value = amount || 0;
+  return new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value);
+};
