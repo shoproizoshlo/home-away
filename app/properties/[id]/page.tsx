32ffa5266f5e348c8fd06b4ffaa2d1494d9a592b
shoproizoshlo <iamsusunny@gmail.com>
2024-08-12 15:28:47 +0200

    Property Map Component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -11,6 +11,16 @@ import UserInfo from "@/components/properties/UserInfo";
 import { Separator } from "@radix-ui/react-dropdown-menu";
 import Description from "@/components/properties/Description";
 import Amenities from "@/components/properties/Amenities";
+import { Skeleton } from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
+
+const DynamicMap = dynamic(
+  () => import("@/components/properties/PropertyMap"),
+  {
+    ssr: false,
+    loading: () => <Skeleton className="h-[400px] w-full" />,
+  }
+);
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -43,6 +53,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <Separator className="mt-4" />
           <Description description={property.description} />
           <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />{" "}
         </div>
         <div className="lg:col-span-4 flex flex-col items-center">
           <BookingCalendar />
