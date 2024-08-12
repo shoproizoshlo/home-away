3df0ea892677938786e83a4248597c9a8d04feb0
shoproizoshlo <iamsusunny@gmail.com>
2024-08-12 14:23:48 +0200

    Breadcrumbs Component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,5 @@
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -6,6 +8,18 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect("/");
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  return <div>PropertyDetailsPage</div>;
+
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold ">{property.tagline}</h1>
+        <div className="flex items-center gap-x-4">
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
 }
 export default PropertyDetailsPage;
