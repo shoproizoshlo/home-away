b5ddbdacd2716e667210509741f71944d69104dd
shoproizoshlo <iamsusunny@gmail.com>
2024-08-12 16:13:17 +0200

    Reviews Model

--- utils/actions.ts
+++ utils/actions.ts
@@ -263,3 +263,19 @@ export const fetchPropertyDetails = (id: string) => {
     },
   });
 };
+
+export const createReviewAction = async () => {
+  return { message: "create review" };
+};
+
+export const fetchPropertyReviews = async () => {
+  return { message: "fetch reviews" };
+};
+
+export const fetchPropertyReviewsByUser = async () => {
+  return { message: "fetch user reviews" };
+};
+
+export const deleteReviewAction = async () => {
+  return { message: "delete  reviews" };
+};
