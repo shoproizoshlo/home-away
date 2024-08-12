affe0dc459890f219eb778d5b89d4d9488a658d4
shoproizoshlo <iamsusunny@gmail.com>
2024-08-12 14:39:40 +0200

    Image Container Component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -3,6 +3,8 @@ import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import { fetchPropertyDetails } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import ShareButton from "@/components/properties/ShareButton";
+import ImageContainer from "@/components/properties/ImageContainer";
+import PropertyRating from "@/components/card/PropertyRating";
 
 async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const property = await fetchPropertyDetails(params.id);
@@ -20,6 +22,18 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <FavoriteToggleButton propertyId={property.id} />
         </div>
       </header>
+      <ImageContainer mainImage={property.image} name={property.name} />
+      <section className="lg:grid lg:grid-cols-12 gap-x-12 mt-12">
+        <div className="lg:col-span-8">
+          <div className="flex gap-x-4 items-center">
+            <h1 className="text-xl font-bold">{property.name}</h1>
+            <PropertyRating inPage propertyId={property.id} />
+          </div>
+        </div>
+        <div className="lg:col-span-4 flex flex-col items-center">
+          {/* calendar */}
+        </div>
+      </section>
     </section>
   );
 }
