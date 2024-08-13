62259d6e14884adc1419f76e263c131c3a801828
shoproizoshlo <iamsusunny@gmail.com>
2024-08-13 16:21:41 +0200

    Fetch Bookings and Delete Booking

--- utils/format.ts
+++ utils/format.ts
@@ -11,3 +11,11 @@ export const formatCurrency = (amount: number | null) => {
 export function formatQuantity(quantity: number, noun: string): string {
   return quantity === 1 ? `${quantity} ${noun}` : `${quantity} ${noun}s`;
 }
+
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+};
