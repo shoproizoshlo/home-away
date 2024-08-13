1678820ee2dbdf99e5fa4f8a5585b9be763f8501
shoproizoshlo <iamsusunny@gmail.com>
2024-08-13 12:38:32 +0200

    Fetch and Delete User Reviews

--- utils/actions.ts
+++ utils/actions.ts
@@ -306,3 +306,43 @@ export async function fetchPropertyReviews(propertyId: string) {
   });
   return reviews;
 }
+
+export const fetchPropertyReviewsByUser = async () => {
+  const user = await getAuthUser();
+  const reviews = await db.review.findMany({
+    where: {
+      profileId: user.id,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      property: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+  });
+  return reviews;
+};
+
+export const deleteReviewAction = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+  const user = await getAuthUser();
+
+  try {
+    await db.review.delete({
+      where: {
+        id: reviewId,
+        profileId: user.id,
+      },
+    });
+
+    revalidatePath("/reviews");
+    return { message: "Review deleted successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
