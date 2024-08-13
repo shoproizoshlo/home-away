44615860ea9febedff174c55288705a9f0b06030
shoproizoshlo <iamsusunny@gmail.com>
2024-08-13 15:55:34 +0200

    Create Booking Action

--- utils/actions.ts
+++ utils/actions.ts
@@ -11,6 +11,7 @@ import {
   validateWithZodSchema,
 } from "./schemas";
 import { uploadImage } from "./supabase";
+import { calculateTotals } from "./calculateTotals";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -386,6 +387,40 @@ export const findExistingReview = async (
   });
 };
 
-export const createBookingAction = async () => {
-  return { message: "create booking" };
+export const createBookingAction = async (prevState: {
+  propertyId: string;
+  checkIn: Date;
+  checkOut: Date;
+}) => {
+  const user = await getAuthUser();
+
+  const { propertyId, checkIn, checkOut } = prevState;
+  const property = await db.property.findUnique({
+    where: { id: propertyId },
+    select: { price: true },
+  });
+  if (!property) {
+    return { message: "Property not found" };
+  }
+  const { orderTotal, totalNights } = calculateTotals({
+    checkIn,
+    checkOut,
+    price: property.price,
+  });
+
+  try {
+    const booking = await db.booking.create({
+      data: {
+        checkIn,
+        checkOut,
+        orderTotal,
+        totalNights,
+        profileId: user.id,
+        propertyId,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect("/bookings");
 };
