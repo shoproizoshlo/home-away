c1f988925c8cfb70f41313859ba38c02910a3371
shoproizoshlo <iamsusunny@gmail.com>
2024-08-13 13:12:14 +0200

    PropertyRating Component - Complete

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,3 +1,4 @@
+import { fetchPropertyRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
 async function PropertyRating({
@@ -7,12 +8,10 @@ async function PropertyRating({
   propertyId: string;
   inPage: boolean;
 }) {
-  // temp
-  const rating = 4.7;
-  const count = 100;
-
+  const { rating, count } = await fetchPropertyRating(propertyId);
+  if (count === 0) return null;
   const className = `flex gap-1 items-center ${inPage ? "text-md" : "text-xs"}`;
-  const countText = count > 1 ? "reviews" : "review";
+  const countText = count === 1 ? "review" : "reviews";
   const countValue = `(${count}) ${inPage ? countText : ""}`;
   return (
     <span className={className}>
--- utils/actions.ts
+++ utils/actions.ts
@@ -346,3 +346,24 @@ export const deleteReviewAction = async (prevState: { reviewId: string }) => {
     return renderError(error);
   }
 };
+
+export async function fetchPropertyRating(propertyId: string) {
+  const result = await db.review.groupBy({
+    by: ["propertyId"],
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+    where: {
+      propertyId,
+    },
+  });
+
+  // empty array if no reviews
+  return {
+    rating: result[0]?._avg.rating?.toFixed(1) ?? 0,
+    count: result[0]?._count.rating ?? 0,
+  };
+}
