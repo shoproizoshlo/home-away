fddf854eb2183bc5081facc7637d4259125ef8c0
shoproizoshlo <iamsusunny@gmail.com>
2024-08-13 17:13:57 +0200

    Edit Rentals Page

--- utils/actions.ts
+++ utils/actions.ts
@@ -528,3 +528,22 @@ export async function deleteRentalAction(prevState: { propertyId: string }) {
     return renderError(error);
   }
 }
+
+export const fetchRentalDetails = async (propertyId: string) => {
+  const user = await getAuthUser();
+
+  return db.property.findUnique({
+    where: {
+      id: propertyId,
+      profileId: user.id,
+    },
+  });
+};
+
+export const updatePropertyAction = async () => {
+  return { message: "update property action" };
+};
+
+export const updatePropertyImageAction = async () => {
+  return { message: "update property image" };
+};
