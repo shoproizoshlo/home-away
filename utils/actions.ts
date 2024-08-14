ca607a3e7a52e0e80603c3974a195651adc98049
shoproizoshlo <iamsusunny@gmail.com>
2024-08-14 12:32:24 +0200

    Admin Page - Fetch Chart Data

--- utils/actions.ts
+++ utils/actions.ts
@@ -12,6 +12,7 @@ import {
 } from "./schemas";
 import { uploadImage } from "./supabase";
 import { calculateTotals } from "./calculateTotals";
+import { formatDate } from "./format";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -641,4 +642,34 @@ export const fetchStats = async () => {
     propertiesCount,
     bookingsCount,
   };
+};
+
+export const fetchChartsData = async () => {
+  await getAdminUser();
+  const date = new Date();
+  date.setMonth(date.getMonth() - 6);
+  const sixMonthsAgo = date;
+
+  const bookings = await db.booking.findMany({
+    where: {
+      createdAt: {
+        gte: sixMonthsAgo,
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+  let bookingsPerMonth = bookings.reduce((total, current) => {
+    const date = formatDate(current.createdAt, true);
+
+    const existingEntry = total.find((entry) => entry.date === date);
+    if (existingEntry) {
+      existingEntry.count += 1;
+    } else {
+      total.push({ date, count: 1 });
+    }
+    return total;
+  }, [] as Array<{ date: string; count: number }>);
+  return bookingsPerMonth;
 };
--- utils/format.ts
+++ utils/format.ts
@@ -1,3 +1,16 @@
+export const formatDate = (date: Date, onlyMonth?: boolean) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+  };
+
+  if (!onlyMonth) {
+    options.day = "numeric";
+  }
+
+  return new Intl.DateTimeFormat("de-DE", options).format(date);
+};
+
 export const formatCurrency = (amount: number | null) => {
   const value = amount || 0;
   return new Intl.NumberFormat("de-DE", {
@@ -11,11 +24,3 @@ export const formatCurrency = (amount: number | null) => {
 export function formatQuantity(quantity: number, noun: string): string {
   return quantity === 1 ? `${quantity} ${noun}` : `${quantity} ${noun}s`;
 }
-
-export const formatDate = (date: Date) => {
-  return new Intl.DateTimeFormat("de-DE", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(date);
-};
