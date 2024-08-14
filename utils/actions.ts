c691f3227bfee833323e088d60c19acbd1a37273
shoproizoshlo <iamsusunny@gmail.com>
2024-08-14 13:31:25 +0200

    Stripe - Checkout Page

--- utils/actions.ts
+++ utils/actions.ts
@@ -400,6 +400,7 @@ export const createBookingAction = async (prevState: {
   checkOut: Date;
 }) => {
   const user = await getAuthUser();
+  let bookingId: null | string = null;
 
   const { propertyId, checkIn, checkOut } = prevState;
   const property = await db.property.findUnique({
@@ -426,10 +427,11 @@ export const createBookingAction = async (prevState: {
         propertyId,
       },
     });
+    bookingId = booking.id;
   } catch (error) {
     return renderError(error);
   }
-  redirect("/bookings");
+  redirect(`/checkout?bookingId=${bookingId}`);
 };
 
 export const fetchBookings = async () => {
